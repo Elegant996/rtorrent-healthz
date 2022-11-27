028bd88185d8d270fb6c1833fa91d76e3c51cfda
Elegant996 <user@elegant996.net>
2022-11-28 04:19:00 +0900

    Overhaul
    Re-use RPC client
    Switch to Interface objects
    Add additional args

--- src/go.mod
+++ src/go.mod
@@ -6,11 +6,11 @@ require (
 	github.com/Elegant996/scgi v1.0.0
 	github.com/Elegant996/xmlrpc v0.0.0-20200426092339-6d7edd4ac412
 	github.com/creachadair/jrpc2 v0.42.0
-	k8s.io/klog v1.0.0
+	k8s.io/klog/v2 v2.80.1
 )
 
 require (
+	github.com/go-logr/logr v1.2.0 // indirect
 	github.com/keegancsmith/rpc v1.3.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/text v0.3.6 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -9,124 +9,51 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/Elegant996/scgi"
-	"github.com/Elegant996/xmlrpc"
 	"github.com/creachadair/jrpc2"
 	"github.com/creachadair/jrpc2/jhttp"
+	"github.com/Elegant996/scgi"
+	"github.com/Elegant996/xmlrpc"
 
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 )
 
 // Command line flags
 var (
-	probeTimeout = flag.Duration("probe-timeout", time.Second, "Probe timeout in seconds.")
-	scgiAddress  = flag.String("scgi-address", "/run/rtorrent/rtorrent.sock", "Address of the SCGI socket.")
-	httpEndpoint = flag.String("http-endpoint", "", "The TCP network address where the HTTP server for diagnostics, including SCGI server health check and save requests. The default is empty string, which means the server is disabled.")
+	operationTimeout	= flag.Duration("timeout", time.Second, "Timeout for waiting for communication with rtorrent.")
+	scgiAddress			= flag.String("scgi-address", "/run/scgi/socket", "Path of the SCGI server socket that the rtorrent-healthz will connect to.")
+	httpEndpoint		= flag.String("http-endpoint", "", "The TCP network address where the HTTP server for diagnostics, including SCGI server health check and save requests. The default is empty string, which means the server is disabled.")
+	encoding			= flag.String("encoding", "xml", "The encoding mechanism used for remote procedure calling.")
+
+	// List of supported versions
+	supportedVersions = []string{"0.9.8"}
 )
 
-type scgiSession struct {
-	sessionName string
-	jsonSupport bool
+// rpcCodec contains an RPC client with the desired encoding
+type rpcCodec struct {
+	client interface{}
 }
 
-func (s *scgiSession) checkHealthJson(w http.ResponseWriter, req *http.Request) {
-	ctx, cancel := context.WithTimeout(req.Context(), *probeTimeout)
-	defer cancel()
-
-	ch := jhttp.NewChannel(*scgiAddress, &jhttp.ChannelOptions{Client: &http.Client{Transport: &scgi.Transport{}}})
-	cli := jrpc2.NewClient(ch, nil)
-	defer cli.Close()
-
-	klog.V(5).Infof("Sending probe request to SCGI session %q", s.sessionName)
-	var result string
-	if err := cli.CallResult(ctx, "system.api_version", []string{""}, &result); err != nil {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		w.Write([]byte(err.Error()))
-		klog.Errorf("health check failed: %v", err)
-		return
+// newRPCCodec returns an initialized rpcCodec instance
+func newRPCCodec() *rpcCodec {
+	switch *encoding {
+	case "json":
+		ch		:= jhttp.NewChannel(*scgiAddress, &jhttp.ChannelOptions{Client: &http.Client{Transport: &scgi.Transport{}}})
+		c		:= jrpc2.NewClient(ch, nil)
+		return &rpcCodec{client: c}
+	default: // xml
+		c, _	:= xmlrpc.NewClient(*scgiAddress, &scgi.Transport{})
+		return &rpcCodec{client: c}
 	}
-
-	klog.V(5).Infof("system.api_version = %s", result)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`ok`))
-	klog.V(5).Infof("Health check succeeded")
 }
 
-func (s *scgiSession) saveSessionJson(w http.ResponseWriter, req *http.Request) {
-	ch := jhttp.NewChannel(*scgiAddress, &jhttp.ChannelOptions{Client: &http.Client{Transport: &scgi.Transport{}}})
-	cli := jrpc2.NewClient(ch, nil)
-	defer cli.Close()
-
-	klog.V(5).Infof("Sending save request to SCGI session %q", s.sessionName)
-	var result int
-	if err := cli.CallResult(context.Background(), "session.save", []string{""}, &result); err != nil {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		w.Write([]byte(err.Error()))
-		klog.Errorf("session save failed: %v", err)
-		return
+// Call is the RPC invoked with desired encoding mechanism
+func (r *rpcCodec) Call(ctx context.Context, method string, params any, result any) error {
+	switch r.client.(type) {
+	case *jrpc2.Client:
+		return r.client.(*jrpc2.Client).CallResult(ctx, method, []string{""}, result)
+	default: // *xmlrpc.Client
+		return r.client.(*xmlrpc.Client).Call(ctx, method, params, result)
 	}
-
-	klog.V(5).Infof("session.save = %d", result)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`ok`))
-	klog.V(5).Infof("Session save succeeded")
-}
-
-func (s *scgiSession) checkHealthXml(w http.ResponseWriter, req *http.Request) {
-	ctx, cancel := context.WithTimeout(req.Context(), *probeTimeout)
-	defer cancel()
-
-	cli, err := xmlrpc.NewClient(*scgiAddress, &scgi.Transport{})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		klog.Errorf("failed to establish connection to SCGI server: %v", err)
-		return
-	}
-	defer cli.Close()
-
-	klog.V(5).Infof("Sending probe request to SCGI session %q", s.sessionName)
-	var result string
-	if err := cli.Call(ctx, "system.api_version", nil, &result); err != nil {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		w.Write([]byte(err.Error()))
-		klog.Errorf("health check failed: %v", err)
-		return
-	}
-
-	klog.V(5).Infof("system.api_version = %s", result)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`ok`))
-	klog.V(5).Infof("Health check succeeded")
-}
-
-func (s *scgiSession) saveSessionXml(w http.ResponseWriter, req *http.Request) {
-	cli, err := xmlrpc.NewClient(*scgiAddress, &scgi.Transport{})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		klog.Errorf("failed to establish connection to SCGI server: %v", err)
-		return
-	}
-	defer cli.Close()
-
-	klog.V(5).Infof("Sending save request to SCGI session %q", s.sessionName)
-	var result int
-	if err := cli.Call(context.Background(), "session.save", nil, &result); err != nil {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		w.Write([]byte(err.Error()))
-		klog.Errorf("session save failed: %v", err)
-		return
-	}
-
-	klog.V(5).Infof("session.save = %d", result)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`ok`))
-	klog.V(5).Infof("Session save succeeded")
 }
 
 func main() {
@@ -134,64 +61,75 @@ func main() {
 	flag.Set("logtostderr", "true")
 	flag.Parse()
 
-	ch := jhttp.NewChannel(*scgiAddress, &jhttp.ChannelOptions{Client: &http.Client{Transport: &scgi.Transport{}}})
-	sjcli := jrpc2.NewClient(ch, nil)
+	klog.Infof("Running rtorrent-healthz with encoding=%s", *encoding)
 
-	klog.Infof("calling SCGI server to discover session name")
-	ss := &scgiSession{jsonSupport: true}
-	if err := sjcli.CallResult(context.Background(), "session.name", []string{""}, &ss.sessionName); err != nil {
-		klog.Warningf("JRPC2 not supported, falling back to XMLRPC: %v", err)
-		ss.jsonSupport = false
-		
-		sxcli, err := xmlrpc.NewClient(*scgiAddress, &scgi.Transport{})
-		if err != nil {
-			klog.Errorf("failed to establish connection to SCGI server: %v", err)
-		}
-		
-		if err := sxcli.Call(context.Background(), "session.name", nil, &ss.sessionName); err != nil {
-			klog.Fatalf("failed to get SCGI session name: %v", err)
-		}
-		sxcli.Close()
+	r := newRPCCodec()
+
+	klog.V(1).Infof("Calling SCGI server to discover session name")
+
+	var scgiSessionName string
+	if err := r.Call(context.Background(), "session.name", nil, &scgiSessionName); err != nil {
+		klog.Errorf("error retrieving SCGI session name: %v", err)
+		os.Exit(1)
 	}
-	sjcli.Close()
 	
-	klog.Infof("SCGI session name: %q", ss.sessionName)
+	klog.V(2).Infof("SCGI session name: %q", scgiSessionName)
 
+	httpServer(r)
+	go removeRegSocket()
+
+	// If RPC server is gracefully shutdown, cleanup and exit
+	os.Exit(0)
+}
+
+func httpServer(r *rpcCodec) {
+	if *httpEndpoint == "" {
+		klog.Infof("Skipping HTTP server because endpoint is set to: %q", httpEndpoint)
+		return
+	}
+	klog.Infof("Starting HTTP server at endpoint: %v\n", httpEndpoint)
+
+	ctx, cancel := context.WithTimeout(context.Background(), *operationTimeout)
+	defer cancel()
+
+	// Prepare http endpoint for healthz + profiling (if enabled)
 	mux := http.NewServeMux()
-	addr := *httpEndpoint
-	if ss.jsonSupport {
-		mux.HandleFunc("/healthz", ss.checkHealthJson)
-		mux.HandleFunc("/save", ss.saveSessionJson)
-	} else {
-		mux.HandleFunc("/healthz", ss.checkHealthXml)
-		mux.HandleFunc("/save", ss.saveSessionXml)
-	}
-	srv := &http.Server {
-		Addr: addr,
-		Handler: mux,
-	}
-
-    done := make(chan bool, 1)
-	quit := make(chan os.Signal, 1)
-
-	signal.Notify(quit, syscall.SIGTERM)
-
-	go func() {
-		<-quit
-		klog.Infof("Stopping requests on: %q", addr)
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-		
-		srv.SetKeepAlivesEnabled(false)
-		if err := srv.Shutdown(ctx); err != nil {
-			klog.Fatalf("failed to shutdown http server with error: %v", err)
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		var result any
+		if err := r.Call(ctx, "system.api_version", nil, &result); err != nil {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`ok`))
+			klog.V(5).Infof("health check succeeded")
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			klog.Errorf("health check failed: %+v", err)
 		}
-		close(done)
-	}()
-	
-	klog.Infof("ServeMux listening at %q", addr)
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		klog.Fatalf("failed to start http server with error: %v", err)
+	})
+	mux.HandleFunc("/save", func(w http.ResponseWriter, req *http.Request) {
+		var result any
+		if err := r.Call(context.Background(), "session.save", nil, &result); err != nil {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`ok`))
+			klog.V(5).Infof("health check succeeded")
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			klog.Errorf("health check failed: %+v", err)
+		}
+	})
+
+	klog.Fatal(http.ListenAndServe(*httpEndpoint, mux))
+}
+
+func removeRegSocket() {
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGTERM)
+	<-sigc
+	err := os.Remove(*scgiAddress)
+	if err != nil && !os.IsNotExist(err) {
+		klog.Errorf("failed to remove socket: %s with error: %+v", *scgiAddress, err)
+		os.Exit(1)
 	}
-	<-done
+	os.Exit(0)
 }
