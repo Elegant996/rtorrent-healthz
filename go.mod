ed5db78ad6a64292cc25f587dc5fe9ee21a915f4
Elegant <user@elegant996.net>
2025-05-07 02:30:51 +0800

    feat: re-write health probe checks

--- go.mod
+++ go.mod
@@ -5,18 +5,18 @@ go 1.24
 toolchain go1.24.2
 
 require (
-	github.com/Elegant996/scgi v1.3.2
+	github.com/Elegant996/scgi v1.3.3
 	github.com/Elegant996/xmlrpc v0.0.0-20200426092339-6d7edd4ac412
 	github.com/creachadair/jrpc2 v1.0.0
 	go.uber.org/zap v1.27.0
 )
 
 require (
-	github.com/creachadair/mds v0.0.1 // indirect
+	github.com/creachadair/mds v0.24.2 // indirect
 	github.com/jub0bs/iterutil v0.5.1 // indirect
 	github.com/keegancsmith/rpc v1.3.0 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,6 +11,10 @@ import (
 	"time"
 
 	"go.uber.org/zap"
+)
+
+const (
+	defaultHealthzPort = "9808"
 )
 
 // Command line flags
@@ -22,6 +27,30 @@ var (
 )
 
 var logger *zap.Logger
+
+type healthProbe struct {
+	sessionName string
+	client      *clientCodec
+}
+
+func (h *healthProbe) checkProbe(w http.ResponseWriter, req *http.Request) {
+	ctx, cancel := context.WithTimeout(req.Context(), *probeTimeout)
+	defer cancel()
+
+	var result any
+	if err := h.client.Call(ctx, "system.pid", nil, &result); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		logger.Error("Health check failed",
+			zap.Error(err),
+		)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`ok`))
+	logger.Debug("Health check succeeded")
+}
 
 func main() {
 	flag.Parse()
@@ -37,82 +66,59 @@ func main() {
 		zap.String("encoding", *encoding),
 	)
 
-	ctx, cancel := context.WithTimeout(context.Background(), *probeTimeout)
-	defer cancel()
+	var addr string
+	if *httpEndpoint != "" {
+		addr = *httpEndpoint
+	} else {
+		addr = net.JoinHostPort("0.0.0.0", defaultHealthzPort)
+	}
 
+	ctx := context.Background()
 	client := newClientCodec()
 
 	logger.Info("Calling SCGI server to discover session name")
 
-	var scgiSessionName string
-	if err := client.Call(ctx, "session.name", nil, &scgiSessionName); err != nil {
+	// Connect to the SCGI server without any timeout to avoid crashing the probe when the server is not ready yet.
+	// Goal: liveness probe never crashes, it only fails the probe when the server is not available (yet).
+	// Since a http server for the probe is not running at this point, Kubernetes liveness probe will fail immediately
+	// with "connection refused", which is good enough to fail the probe.
+	var sessionName string
+	if err := client.Call(ctx, "session.name", nil, &sessionName); err != nil {
 		logger.Error("Cannot rerieve SCGI session name",
 			zap.String("session", ""),
 			zap.Error(err),
 		)
 		os.Exit(1)
 	}
-
 	logger.Info("SCGI session name captured",
-		zap.String("session", scgiSessionName),
+		zap.String("session", sessionName),
+	)
+
+	hp := &healthProbe{
+		sessionName: sessionName,
+		client:      client,
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", hp.checkProbe)
+	logger.Info("ServeMux listening",
+		zap.String("address", addr),
+	)
+
+	logger.Fatal("Failed to start http server",
+		zap.Error(http.ListenAndServe(addr, mux)),
 	)
 
 	done := make(chan bool, 1)
-	go httpServer(client)
-	go removeRegSocket()
+	go removeSocket()
 	<-done
 
 	// If RPC server is gracefully shutdown, cleanup and exit
 	os.Exit(0)
 }
 
-func httpServer(c *clientCodec) {
-	if *httpEndpoint == "" {
-		logger.Info("Skipping HTTP server",
-			zap.String("endpoint", *httpEndpoint),
-		)
-		return
-	}
-	logger.Info("Starting HTTP server",
-		zap.String("endpoint", *httpEndpoint),
-	)
-
-	// Prepare http endpoint for healthz
-	var result any
-	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		if err := c.Call(r.Context(), "system.pid", nil, &result); err != nil {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("ok"))
-			logger.Debug("Health check succeeded")
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			logger.Debug("Health check failed",
-				zap.Error(err),
-			)
-		}
-	})
-	mux.HandleFunc("/save", func(w http.ResponseWriter, r *http.Request) {
-		if err := c.Call(r.Context(), "session.save", nil, &result); err != nil {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("ok"))
-			logger.Debug("Session save succeeded")
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			logger.Debug("Session save failed",
-				zap.Error(err),
-			)
-		}
-	})
-
-	logger.Fatal("HTTP server closed",
-		zap.Error(http.ListenAndServe(*httpEndpoint, mux)),
-	)
-}
-
-func removeRegSocket() {
+func removeSocket() {
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, syscall.SIGTERM)
 	<-sigc
